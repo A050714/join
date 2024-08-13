097ee8acb22c7813af5ef220bde2e23177c549a1
A050714 <129737785+A050714@users.noreply.github.com>
2024-08-13 16:22:02 +0200

    delete Contact

--- js/contacts.js
+++ js/contacts.js
@@ -16,13 +16,13 @@ async function loadContacts() {
             }
         )
     }
-   
+
 }
 
 
 //sorts contacts by name
 function sortContacts() {
-    contacts.sort(function(a, b) {
+    contacts.sort(function (a, b) {
         if (!a.user) return 1;
         if (!b.user) return -1;
         if (a.user.name < b.user.name) {
@@ -44,7 +44,7 @@ async function renderAllContacts() {
     sortContacts();
     let content = document.getElementById('contactList');
     content.innerHTML = '';
-    for (let index = 0; index < contacts.length-1; index++) { 
+    for (let index = 0; index < contacts.length - 1; index++) {
         let contact = contacts[index];
         content.innerHTML += `
             <div class="singleContacts" id="Id_${contact.id}" onclick="showContact(${index})"> 
@@ -59,44 +59,29 @@ async function renderAllContacts() {
 
 function togglePopup() {
 
-    let overlay = document.getElementById('overlay'); 
-    overlay.classList.toggle('show'); 
-    overlay.classList.toggle("hidden"); 
+    let overlay = document.getElementById('overlay');
+    overlay.classList.toggle('show');
+    overlay.classList.toggle("hidden");
 
 }
 
 
 
 //ADD CONTACT//----------------------------------------------------------------------
-<<<<<<< HEAD
 
- async function createContact() {
-     let name = document.getElementById('newName').value;
-     let mail = document.getElementById('newMail').value;
-     let phone = document.getElementById('newPhone').value
-     let userrespone = await getData("Contacts")|| {};
-     let UserKeysArray = Object.keys(userrespone);
-     let userIndex = UserKeysArray.length - 1;
-     await postData(`Contacts/${userIndex}`, { "name": name, "mail": mail, "phone": phone })
-     document.getElementById('newName').value = "";
-     document.getElementById('newMail').value = "";
-     document.getElementById('newPhone').value = "";
- }
-=======
-// async function createContact() {
-//     let name = document.getElementById('newName').value;
-//     let mail = document.getElementById('newMail').value;
-//     let phone = document.getElementById('newPhone').value;
-//     let userrespone = await getData("Contacts")|| {};
-//     let UserKeysArray = Object.keys(userrespone);
-//     let userIndex = UserKeysArray.length - 1;
-
-//     await postData(`Contacts/${userIndex}`, { "name": name, "mail": mail, "phone": phone })
-//     document.getElementById('newName').value = "";
-//     document.getElementById('newMail').value = "";
-//     document.getElementById('newPhone').value = "";
-// }
->>>>>>> 6f3b3e5c5d578b37ba9ff9fdde45c9f73ac6b313
+async function createContact() {
+    let name = document.getElementById('newName').value;
+    let mail = document.getElementById('newMail').value;
+    let phone = document.getElementById('newPhone').value
+    let userrespone = await getData("Contacts") || {};
+    let UserKeysArray = Object.keys(userrespone);
+    let userIndex = UserKeysArray.length;
+    await postData(`Contacts/${userIndex}`, { "name": name, "mail": mail, "phone": phone })
+    document.getElementById('newName').value = "";
+    document.getElementById('newMail').value = "";
+    document.getElementById('newPhone').value = "";
+    location.reload();
+}
 
 
 //DELETE CONTACT//-----------------------------------------------------------------------------
@@ -107,18 +92,23 @@ function findIndexById(contacts, idToFind) {
 
 
 async function deleteContact() {
-    let contactID = document.getElementById('inputID').value; //.value wird später durch onclick(detailCardId) überflüssig
-    await deleteData("Contacts/" + contactID)
-    let index = findIndexById(contacts, contactID);
+    let contactName = document.getElementById('contactName').innerHTML;
+    let firebaseID = contacts.find(x => x.user && x.user.name === contactName).id;
+    console.log(contactName);
+    console.log(firebaseID);
+    await deleteData("Contacts/" + firebaseID)
+    let index = findIndexById(contacts, firebaseID);
     contacts.splice(index, 1)
+    location.reload();
 }
+
 
 //SHOW CONTACT//---------------------------------------------------------------------------------------------
 function showContact(index) {
     document.getElementById('showContact').classList.remove('dNone')
     let contact = contacts[index];
     let contactCardName = document.getElementById('contactName');
-    let contactCardMail = document.getElementById('contactMail'); 
+    let contactCardMail = document.getElementById('contactMail');
     let contactCardPhone = document.getElementById('contactPhone');
     contactCardName.innerHTML = `${contact.user.name}`;
     contactCardMail.innerHTML = `${contact.user.mail}`;
