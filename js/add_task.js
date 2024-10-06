0ac2f6b91e6e10581cf820fc52a60eacb921b3a7
Hyusein Yashar <hyashar96@gmail.com>
2024-10-06 19:42:06 +0200

    asd

--- js/add_task.js
+++ js/add_task.js
@@ -19,7 +19,6 @@ let selectedContacts = [];
  * @function onloadAddTask
  */
 async function onloadAddTask() {
-  checkLog();
   await onloadMain();
   await showFirstLetter();
   contactsDropdown("contactList-a", "selectedContactsDisplay");
